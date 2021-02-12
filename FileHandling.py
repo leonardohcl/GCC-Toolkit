34836a8363559f09a103bc6060484b7654cdd34c
Leonardo Longo <leonardohcl@hotmail.com>
2021-02-11 21:20:28 -0300

    feat: Added Arff class feat: Added combine Arff

--- FileHandling.py
+++ FileHandling.py
@@ -2,6 +2,26 @@ from PIL import Image
 from pandas import DataFrame
 import os 
 
+class Arff:
+    """Structure that holds an arff file
+    
+        Attrs:
+            relation (string): File content description.
+            attrs (list of strings): List of attribute names.
+            attr_types (list of strings): List of attribute types.
+            entries (list of lists): Arff data.
+            classes (list of int): Classes on the file.
+    """
+    def __init__(self):
+        self.relation = ""
+        self.attrs = []
+        self.attr_types = []
+        self.entries = []
+        self.classes = []
+
+    def __len__(self):
+        return len(self.entries)
+
 def readImage(path):
     """Open an image using PIL"""
     try:
@@ -75,7 +95,7 @@ def createArffFile(file_name: str, content: list, attrs: list, classes: list, ta
     f.write("@RELATION '" + file_name + ".arff'\n\n")
 
     for name in attrs:
-        f.write("@ATTRIBUTE " + name + " REAL\n")
+        f.write("@ATTRIBUTE " + name + " numeric\n")
 
     classes_as_strings = [str(x) for x in classes]
     joined_classes = ",".join(classes_as_strings)
@@ -90,4 +110,103 @@ def createArffFile(file_name: str, content: list, attrs: list, classes: list, ta
     
     f.close()
     
+def readArff(path:str):
+    """Reads the content of an arff file to and arff object
+    
+    Args:
+        path(str): Arff file path.
 
+    Returns:
+        Arff type object
+    """
+    if os.path.isfile(path) == False:
+        raise Exception("File doesn't exist. path: {}".format(path))
+
+    obj = Arff()
+
+    f = open(path, "r")
+    line = f.readline()
+    reading_entries = False
+    while line != '':
+        # skips line breaks
+        if line == "\n":
+            line = f.readline()
+            continue
+
+        # clean text
+        line = line.strip()
+
+        if reading_entries:
+            entries = line.split(",")
+            if len(entries) != (len(obj.attrs) + 1):
+                raise Exception("Found entry with more data than specified attributes!")
+            
+            entry_list = []
+            for idx in range(len(obj.attr_types)):
+                if obj.attr_types[idx].lower() == "numeric" or obj.attr_types[idx].lower() == "real":
+                    entry_list.append(float(entries[idx]))
+                else:
+                    entry_list.append(entries[idx])
+                    
+            #gets class at the end
+            entry_class = int(entries[-1])
+            if entry_class not in obj.classes:
+                raise Exception("Read entry with a class different from the ones especified. \nExpected: {}\nGot: {}".format(obj.classes, entry_class))
+            entry_list.append(entry_class)
+
+            if len(obj) == 0:
+                obj.entries = [entry_list]
+            else:
+                obj.entries.append(entry_list)
+        else:
+            if line.lower().startswith("@relation"):
+                splitted_line = line.split(" ")
+                obj.relation = splitted_line[1].replace("'","")
+            elif line.lower().startswith("@attribute"):
+                splitted_line = line.split(" ")
+                if splitted_line[1].lower() == 'class':
+                    classes = splitted_line[2].replace("{","").replace("}","")
+                    obj.classes = [int(num) for num in classes.split(",")]
+                else:
+                    obj.attrs.append(splitted_line[1])
+                    obj.attr_types.append(splitted_line[2])
+            elif line.lower().startswith("@data"):
+                reading_entries = True
+
+        line = f.readline() 
+    
+    f.close()
+
+    return obj
+
+def mergeArffs(arff1:Arff, arff2: Arff):
+    """Combine two arff objects into one and write it to a target
+
+    Args:
+        arff1(Arff): First arff.
+        arff2(Arff): Second arff.
+
+    Returns:
+        Combined Arff object 
+    """
+    if len(arff1) != len(arff2):
+        raise Exception("Can't combine arffs with different data length.")
+
+    if arff1.classes != arff2.classes:
+        raise Exception("Can't combine arffs with different classes.")
+
+    combined = Arff()
+    combined.classes = arff1.classes
+    combined.relation = "Merged_{}_and_{}".format(arff1.relation,arff2.relation)
+    combined.attrs = arff1.attrs + arff2.attrs
+    combined.attr_types = arff1.attr_types + arff2.attr_types
+    for idx in range(len(arff1)):
+        if arff1.entries[idx][-1] != arff2.entries[idx][-1]:
+            raise Exception("Failed combining entries. Class missmatch at index {}".format(idx))
+        combined_entry = arff1.entries[idx][:-1] + arff2.entries[idx]
+        if len(combined) == 0:
+            combined.entries = [combined_entry]
+        else:
+            combined.entries.append(combined_entry)
+    
+    return combined
